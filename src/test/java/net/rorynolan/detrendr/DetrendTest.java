e91bfc6351ef3f01218f6cb0e92349ea53e41d32
Rory Nolan <rorynoolan@gmail.com>
2018-10-01 10:50:28 +0100

    Can build with [INFO] Scanning for projects... [INFO] [INFO] -----------------------< net.rorynolan:Detrendr >----------------------- [INFO] Building Detrendr 0.0.0.9003-SNAPSHOT [INFO] --------------------------------[ jar ]--------------------------------- [INFO] [INFO] --- build-helper-maven-plugin:1.9.1:regex-property (sanitize-version) @ Detrendr --- [INFO] [INFO] --- buildnumber-maven-plugin:1.3:create (default) @ Detrendr --- [INFO] Executing: /bin/sh -c cd /Users/rnolan/Dropbox/IdeaProjects/ij_detrendr && git rev-parse --verify HEAD [INFO] Working directory: /Users/rnolan/Dropbox/IdeaProjects/ij_detrendr [INFO] Storing buildNumber: db70b47a7b9c137a4cf102e75863826b43c5d621 at timestamp: 1538387427479 [INFO] Storing buildScmBranch: master [INFO] [INFO] --- maven-resources-plugin:2.6:resources (default-resources) @ Detrendr --- [INFO] Using 'UTF-8' encoding to copy filtered resources. [INFO] Copying 0 resource [INFO] [INFO] --- maven-compiler-plugin:3.1:compile (default-compile) @ Detrendr --- [INFO] Nothing to compile - all classes are up to date [INFO] [INFO] --- maven-resources-plugin:2.6:testResources (default-testResources) @ Detrendr --- [INFO] Not copying test resources [INFO] [INFO] --- maven-compiler-plugin:3.1:testCompile (default-testCompile) @ Detrendr --- [INFO] Not compiling test sources [INFO] [INFO] --- maven-surefire-plugin:2.22.0:test (default-test) @ Detrendr --- [INFO] Tests are skipped. [INFO] [INFO] --- maven-jar-plugin:2.5:jar (default-jar) @ Detrendr --- [INFO] Building jar: /Users/rnolan/Dropbox/IdeaProjects/ij_detrendr/target/Detrendr-0.0.0.9003-SNAPSHOT.jar [INFO] [INFO] --- maven-source-plugin:2.3:jar-no-fork (default) @ Detrendr --- [INFO] Building jar: /Users/rnolan/Dropbox/IdeaProjects/ij_detrendr/target/Detrendr-0.0.0.9003-SNAPSHOT-sources.jar [INFO] [INFO] --- maven-jar-plugin:2.5:test-jar (default) @ Detrendr --- [INFO] Skipping packaging of the test-jar [INFO] ------------------------------------------------------------------------ [INFO] BUILD SUCCESS [INFO] ------------------------------------------------------------------------ [INFO] Total time: 1.861 s [INFO] Finished at: 2018-10-01T10:50:28+01:00 [INFO] ------------------------------------------------------------------------.

--- src/test/java/net/rorynolan/detrendr/DetrendTest.java
+++ src/test/java/net/rorynolan/detrendr/DetrendTest.java
@@ -13,7 +13,8 @@ class DetrendTest {
   @Test
   void testDetrendingAndBrightness() {
     try {
-      ImagePlus img = new ImagePlus("https://github.com/rorynolan/nandb/raw/master/inst/extdata/50.tif");
+      String nandbImgUrl = "/Library/Frameworks/R.framework/Versions/3.5/Resources/library/nandb/extdata/50.tif";
+      ImagePlus img = new ImagePlus(nandbImgUrl);
       Matrix imgMat = MyImg.convertToMatrix(img);
       double[] frameMeans = MyStats.rowsMeans(imgMat);
       double frameMeansVar = MyStats.var(frameMeans);
@@ -29,6 +30,9 @@ class DetrendTest {
         assertEquals(1.044, detrendedMeanB, 0.002);
       }
     } catch (DataFormatException e) {
+      System.out.println(e);
+      assertEquals("this", "that");
+    } catch (StringIndexOutOfBoundsException e) {
       System.out.println(e);
       assertEquals("this", "that");
     }
